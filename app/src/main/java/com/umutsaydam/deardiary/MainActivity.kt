61f5421e0807891af73bde7e8e3f4db31589cba9
umutsaydam <umutsaydam24@gmail.com>
2025-04-10 19:56:06 +0300

    First commit

---
+++ app/src/main/java/com/umutsaydam/deardiary/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.umutsaydam.deardiary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.umutsaydam.deardiary.ui.theme.DearDiaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DearDiaryTheme {
+
+            }
+        }
+    }
+}
