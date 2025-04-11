2375b55b8981a16b8af33a306a1d444972a2ac64
umutsaydam <umutsaydam24@gmail.com>
2025-04-11 11:57:36 +0300

    Add: Dimens added

--- app/src/main/java/com/umutsaydam/deardiary/presentation/DiariesScreen.kt
+++ app/src/main/java/com/umutsaydam/deardiary/presentation/DiariesScreen.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,11 +17,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.umutsaydam.deardiary.R
 import com.umutsaydam.deardiary.domain.DiaryEntity
-import com.umutsaydam.deardiary.presentation.navigation.Route
+import com.umutsaydam.deardiary.presentation.Dimens.CORNER_MEDIUM
 import java.util.Date
 import java.util.UUID
 
@@ -58,18 +55,8 @@ fun DiariesScreen(navController: NavHostController) {
         },
         containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
+            AddDiaryFab {
 
-                },
-                containerColor = MaterialTheme.colorScheme.surfaceContainer,
-                contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                shape = RoundedCornerShape(16.dp)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_edit_filled),
-                    contentDescription = "Add a diary"
-                )
             }
         },
         bottomBar = {
@@ -78,9 +65,24 @@ fun DiariesScreen(navController: NavHostController) {
     ) { paddingValues ->
         val date = Date()
         val list = listOf(
-            DiaryEntity(UUID.randomUUID(), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.", date, 0),
-            DiaryEntity(UUID.randomUUID(), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.", date, 1),
-            DiaryEntity(UUID.randomUUID(), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.", date, 2),
+            DiaryEntity(
+                UUID.randomUUID(),
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.",
+                date,
+                0
+            ),
+            DiaryEntity(
+                UUID.randomUUID(),
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.",
+                date,
+                1
+            ),
+            DiaryEntity(
+                UUID.randomUUID(),
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id sollicitudin leo. Sed eget justo id sapien accumsan mattis quis nec sapien. Suspendisse commodo sed arcu non volutpat. Integer nulla justo, varius sit amet justo eget, varius molestie purus. Duis in imperdiet diam. Donec eget velit est. Fusce ipsum lacus, semper eget eros vitae, dapibus suscipit lectus. Vestibulum tempus suscipit tellus et elementum. Pellentesque faucibus purus mauris, at dignissim enim aliquet vel.",
+                date,
+                2
+            ),
             DiaryEntity(UUID.randomUUID(), "Content3", date, 3),
             DiaryEntity(UUID.randomUUID(), "Content4", date, 4),
             DiaryEntity(UUID.randomUUID(), "Content5", date, 5),
@@ -97,5 +99,22 @@ fun DiariesScreen(navController: NavHostController) {
             diaryEntityList = list
         )
 
+    }
+}
+
+@Composable
+fun AddDiaryFab(onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick = {
+            onClick()
+        },
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        shape = RoundedCornerShape(CORNER_MEDIUM)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_edit_filled),
+            contentDescription = "Add a diary"
+        )
     }
 }
---
+++ app/src/main/java/com/umutsaydam/deardiary/presentation/Dimens.kt
@@ -0,0 +1,9 @@
+package com.umutsaydam.deardiary.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    val CORNER_SMALL = 5.dp
+    val CORNER_MEDIUM = 16.dp
+}
