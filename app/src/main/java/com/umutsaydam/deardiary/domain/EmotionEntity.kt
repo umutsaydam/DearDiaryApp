6b1eea8aff0e182f687d5db2d5b784187cff9d78
umutsaydam <umutsaydam24@gmail.com>
2025-04-12 13:05:05 +0300

    Add: EmotionEntity created

---
+++ app/src/main/java/com/umutsaydam/deardiary/domain/EmotionEntity.kt
@@ -0,0 +1,18 @@
+package com.umutsaydam.deardiary.domain
+
+import com.umutsaydam.deardiary.R
+
+data class EmotionEntity(
+    val emotionId: Int,
+    val emotionContent: String,
+    val emotionSource: Int
+)
+
+val emotionList = listOf(
+    EmotionEntity(0, "Sad", R.raw.anim_sad),
+    EmotionEntity(1, "Joy", R.raw.anim_joy),
+    EmotionEntity(2, "Love", R.raw.anim_love),
+    EmotionEntity(3, "Anger", R.raw.anim_anger),
+    EmotionEntity(4, "Fear", R.raw.anim_fear),
+    EmotionEntity(5, "Surprise", R.raw.anim_surprise),
+)
