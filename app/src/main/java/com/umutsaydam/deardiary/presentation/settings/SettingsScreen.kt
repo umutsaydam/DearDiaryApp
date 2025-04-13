670e7feb39ba15b456b8946dd55dcb09bfadf10a
umutsaydam <umutsaydam24@gmail.com>
2025-04-13 19:46:22 +0300

    Refactor: SettingsScreen, removed unused libs

--- app/src/main/java/com/umutsaydam/deardiary/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/umutsaydam/deardiary/presentation/settings/SettingsScreen.kt
@@ -3,7 +3,6 @@ package com.umutsaydam.deardiary.presentation.settings
 import android.os.Build
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -28,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
