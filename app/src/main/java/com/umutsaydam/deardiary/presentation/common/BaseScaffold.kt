8db94ead327b749ee4a12fff00bf8c713cc7c016
umutsaydam <umutsaydam24@gmail.com>
2025-04-19 14:53:50 +0300

    Refactor: BaseScaffold, title change as Composable function

--- app/src/main/java/com/umutsaydam/deardiary/presentation/common/BaseScaffold.kt
+++ app/src/main/java/com/umutsaydam/deardiary/presentation/common/BaseScaffold.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -16,7 +15,7 @@ import androidx.compose.ui.graphics.Color
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseScaffold(
-    title: @Composable () -> Unit,
+    title: @Composable () -> Unit = {},
     topActions: @Composable RowScope.() -> Unit = {},
     navigation: @Composable () -> Unit = {},
     fabContent: @Composable () -> Unit = {},
