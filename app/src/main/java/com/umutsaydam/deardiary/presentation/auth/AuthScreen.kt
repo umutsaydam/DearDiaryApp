641da9a20637dbde29434971f05017c7eb67a618
umutsaydam <umutsaydam24@gmail.com>
2025-04-20 18:28:45 +0300

    Refactor: AuthScreen, nav padding was added and strings.xml space added to Sign in now. and Sign up now. texts

--- app/src/main/java/com/umutsaydam/deardiary/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/umutsaydam/deardiary/presentation/auth/AuthScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
@@ -204,7 +205,8 @@ fun AuthScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(bottom = PaddingXSmall),
+            .padding(bottom = PaddingXSmall)
+            .navigationBarsPadding(),
         contentAlignment = Alignment.BottomCenter
     ) {
         ColorfulText(
